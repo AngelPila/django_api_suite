982e368c4d578fc41a29c5065ed237bc7f0da59e
Carlos Luis López Pérez <carlosluislopezcba@gmail.com>
2026-01-08 13:28:28 +0000

    Get Y Post Firebase_api

--- landing_api/views.py
+++ landing_api/views.py
@@ -9,3 +9,29 @@ from rest_framework import status
 class LandingAPI(APIView):
 	name = "Landing API"
 	collection_name = "landing"
+    
+	def get(self, request):
+		# Referencia a la colección
+		ref = db.reference(f'{self.collection_name}')
+
+		# get: Obtiene todos los elementos de la col ección
+		data = ref.get()
+
+		# Devuelve un arreglo JSON
+		return Response(data, status=status.HTTP_200_OK)
+	
+	def post(self, request):
+		data = request.data
+
+		# Referencia a la colección
+		ref = db.reference(f'{self.collection_name}')
+
+		current_time  = datetime.now()
+		custom_format = current_time.strftime("%d/%m/%Y, %I:%M:%S %p").lower().replace('am', 'a. m.').replace('pm', 'p. m.')
+		data.update({"timestamp": custom_format })
+
+		# push: Guarda el objeto en la colección
+		new_resource = ref.push(data)
+
+		# Devuelve el id del objeto guardado
+		return Response({"id": new_resource.key}, status=status.HTTP_201_CREATED)
